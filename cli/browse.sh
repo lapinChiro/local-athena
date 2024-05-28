8f95cdd2319bf10e7c09fbb45896302e8f966f46
ishihara <lapinChiro@gmail.com>
2024-05-28 17:30:14 +0900

    fix bug

--- cli/browse.sh
+++ cli/browse.sh
@@ -2,12 +2,12 @@
 
 browse() {
     local glue_url="http://localhost:8888"
-    local trino_url="http://localhost:9001"
+    local minio_url="http://localhost:9001"
 
     local subcommand=$1; [ $# -gt 0 ] && shift
     case $subcommand in
         "glue"     ) open_browser $glue_url ;;
-        "trino"    ) open_browser $trino_url ;;
+        "minio"    ) open_browser $minio_url ;;
         *          ) usage ;;
     esac
 }
@@ -19,7 +19,7 @@ open_browser() {
 usage() {
     echo 'Commands:' 1>&2
     echo '  browse glue         glue画面をデフォルトブラウザで開く' 1>&2
-    echo '  browse trino        trinoアドミン画面をデフォルトブラウザで開く' 1>&2
+    echo '  browse minio        minioアドミン画面をデフォルトブラウザで開く' 1>&2
 }
 
 browse "$@"
