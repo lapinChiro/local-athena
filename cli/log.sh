abe97b471d24d5aadf21a59caa8a2331e63d07c7
ishihara <lapinChiro@gmail.com>
2024-05-30 09:52:21 +0900

    select出来ない

--- cli/log.sh
+++ cli/log.sh
@@ -1,14 +1,15 @@
-#!/bin/sh 
+#!/bin/sh
 
 log() {
 
-    local subcommand=$1; [ $# -gt 0 ] && shift
+    local subcommand=$1
+    [ $# -gt 0 ] && shift
     case $subcommand in
-        "glue"           ) show_log glue ;;
-        "trino"          ) show_log trino ;;
-        "hive_metastore" ) show_log hive-metastore ;;
-        "minio"          ) show_log minio ;;
-        *                ) usage ;;
+    "glue") show_log glue ;;
+    "trino") show_log trino ;;
+    "hive-metastore") show_log hive-metastore ;;
+    "minio") show_log minio ;;
+    *) usage ;;
     esac
 }
 
@@ -20,7 +21,7 @@ usage() {
     echo 'Commands:' 1>&2
     echo ' log glue               glueコンテナのログを表示する' 1>&2
     echo ' log trino              trinoコンテナのログを表示する' 1>&2
-    echo ' log hive_metastore     hive_metastoreコンテナのログを表示する' 1>&2
+    echo ' log hive-metastore     hive-metastoreコンテナのログを表示する' 1>&2
     echo ' log minio              minioコンテナのログを表示する' 1>&2
 }
 
