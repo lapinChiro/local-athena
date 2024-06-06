39d9b44923488b988e2446a16b19e1a34223a8e9
ishihara <lapinChiro@gmail.com>
2024-06-06 19:46:03 +0900

    sql result with header

--- web/src/trino/simple_request.rs
+++ web/src/trino/simple_request.rs
@@ -5,5 +5,7 @@ pub async fn request(client: Client, sql: &str) -> anyhow::Result<Vec<Vec<Value>
     println!("{sql}");
     let result: Vec<Row> = client.get_all::<Row>(sql.into()).await?.into_vec();
 
+    let result_2 = client.get_all::<Row>(sql.into()).await?.split();
+    println!("{:#?}", result_2);
     Ok(result.iter().map(|row| row.clone().into_json()).collect())
 }
