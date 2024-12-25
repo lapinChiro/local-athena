4778faa2a6e298323946433271e38742ee392515
ishihara <lapinChiro@gmail.com>
2024-12-25 09:20:16 +0900

    rm

--- sample_data/for_asahi/init_bucket.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export AWS_ACCESS_KEY_ID=minioadmin
-export AWS_SECRET_ACCESS_KEY=minioadmin
-export AWS_DEFAULT_REGION=ap-northeast-1
-export MINIO_ENDPOINT=http://localhost:9000
-
-aws s3 mb --endpoint-url $MINIO_ENDPOINT s3://asahi-data
-aws s3 sync --endpoint-url $MINIO_ENDPOINT ./asahi_data/ s3://asahi-data/
